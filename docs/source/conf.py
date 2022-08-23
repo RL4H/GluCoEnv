8d1425eb92cc37e4132647c16baaefaf6815295c
Chirath <chirathyh@gmail.com>
2022-08-23 11:21:56 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GluCoEnv'
+copyright = '2022, Chirath Hettiarachchi'
+author = 'Chirath Hettiarachchi'
 
 release = '0.1'
 version = '0.1.0'
