a6367b24ef6d0af5356c6309ad343a5bcfadd7af
Chirath <chirathyh@gmail.com>
2022-08-23 17:19:49 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ epub_show_urls = 'footnote'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "img/glucoenv.png"
+html_logo = "_static/img/glucoenv.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
